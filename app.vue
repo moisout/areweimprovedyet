55db21bf9bc93e8020c4ea576d477a8bfaae5661
Maurice Oegerli <maurice@oeger.li>
2022-04-28 09:36:51 +0200

    ✨ add blockers

--- app.vue
+++ app.vue
@@ -1,11 +1,46 @@
+<script setup>
+useHead({
+  title: "Are we improved yet?"
+});
+</script>
+
 <template>
-  <div>
-    <h1>No</h1>
-    <div class="blockers">
-      <h2>Blockers</h2>
-      <ul>
-        <li></li>
-      </ul>
+  <main>
+    <div class="center">
+      <h1>No</h1>
+      <div class="blockers">
+        <h2>Blockers</h2>
+        <ul>
+          <li>
+            Windows support [<a href="https://github.com/jwanner83/liferay-npm-bundler-improved/issues/36">#36</a>]
+          </li>
+          <li>JSP gradle build support</li>
+        </ul>
+      </div>
     </div>
-  </div>
+  </main>
 </template>
+
+<style lang="scss">
+html,
+body,
+#__nuxt {
+  margin: 0;
+  height: 100%;
+  width: 100%;
+}
+main {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  font-family: "sans-serif";
+
+  .center {
+    margin: auto;
+    h1 {
+      font-size: 10rem;
+    }
+  }
+}
+</style>
