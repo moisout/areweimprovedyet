7685f8d3d84b60b51bd5f2f49cefbda8551a0bed
Maurice Oegerli <maurice@oeger.li>
2022-04-28 10:20:46 +0200

    ✨ switch to netlify builder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,6 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   nitro: {
-    preset: 'netlify-edge'
+    preset: 'netlify-builder'
   }
 })
